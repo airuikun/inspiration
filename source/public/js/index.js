42fe889374e6250f329c24686ac009fb125bd8e4
Michael Chou <mingzhi22@gmail.com>
2015-08-19 11:41:09 +0800

    格式化代码

--- source/public/js/index.js
+++ source/public/js/index.js
@@ -1,52 +1,50 @@
-var template =
+var template = angular.module('ucpen', [
+    'ngAnimate',
+    'ui.bootstrap',
+    'ui.codemirror'
+]);
 
-angular.module('ucpen', [
-  'ngAnimate',
-  'ui.bootstrap',
-  'ui.codemirror'
-])
+template.value('template', '<!DOCTYPE html><html><head><meta charset="utf-8"><style>{{css}}</style></head><body>{{html}}<script>{{javascript}}</script></body></html>')
 
-.value('template', '<!DOCTYPE html><html><head><meta charset="utf-8"><style>{{css}}</style></head><body>{{html}}<script>{{javascript}}</script></body></html>')
-
-.factory('compile', function (template, $sce) {
-  return function (parts) {
-    var code = template.replace(
-      /{{(.+?)}}/g,
-      function (_, type) {
-        return parts[type]
-      })
-    return $sce.trustAsHtml(code)
-  }
-})
-
-.factory('save', function ($http) {
-  return function (parts) {
-    $http.post('/save', parts).then(function (response) {
-      console.info(response)
-    }).catch(function (response) {
-      console.error(response)
-      alert(response.config.data.html);
-      alert(response.config.data.javascript);
-      alert(response.config.data.css);
+    .factory('compile', function (template, $sce) {
+        return function (parts) {
+            var code = template.replace(
+                /{{(.+?)}}/g,
+                function (_, type) {
+                    return parts[type]
+                });
+            return $sce.trustAsHtml(code)
+        }
     })
-  }
-})
 
-.run(function ($rootScope, $templateRequest, compile, save) {
-  $rootScope.compile = compile
-  $rootScope.save = save
-  $templateRequest('init.html').then(function (data) {
-    $rootScope.html = data
-  })
+    .factory('save', function ($http) {
+        return function (parts) {
+            $http.post('/save', parts).then(function (response) {
+                console.info(response)
+            }).catch(function (response) {
+                console.error(response);
+                alert(response.config.data.html);
+                alert(response.config.data.javascript);
+                alert(response.config.data.css);
+            })
+        }
+    })
 
-  $templateRequest('init.css').then(function (data) {
-    $rootScope.css = data
-  })
+    .run(function ($rootScope, $templateRequest, compile, save) {
+        $rootScope.compile = compile;
+        $rootScope.save = save;
+        $templateRequest('init.html').then(function (data) {
+            $rootScope.html = data
+        });
 
-  $templateRequest('init.js').then(function (data) {
-    $rootScope.javascript = data
-  })
-})
+        $templateRequest('init.css').then(function (data) {
+            $rootScope.css = data
+        });
+
+        $templateRequest('init.js').then(function (data) {
+            $rootScope.javascript = data
+        });
+    });
 
 
 
