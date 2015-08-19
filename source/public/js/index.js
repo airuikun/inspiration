da8dca2a96649c0a66ba12132af897da54d53137
iray198909820 <1853965225@qq.com>
2015-08-19 11:12:27 +0800

    显示提交内容

--- source/public/js/index.js
+++ source/public/js/index.js
@@ -1,84 +1,52 @@
 var template =
 
-    angular.module('ucpen', [
-        'ngAnimate',
-        'ui.bootstrap',
-        'ui.codemirror'
-    ])
+angular.module('ucpen', [
+  'ngAnimate',
+  'ui.bootstrap',
+  'ui.codemirror'
+])
 
-        .value('template', '<!DOCTYPE html><html><head><meta charset="utf-8"><style>{{css}}</style></head><body>{{html}}<script>{{javascript}}</script></body></html>')
+.value('template', '<!DOCTYPE html><html><head><meta charset="utf-8"><style>{{css}}</style></head><body>{{html}}<script>{{javascript}}</script></body></html>')
 
-        .factory('compile', function (template, $sce) {
-            return function (parts) {
-                var code = template.replace(
-                    /{{(.+?)}}/g,
-                    function (_, type) {
-                        return parts[type]
-                    });
-                return $sce.trustAsHtml(code)
-            }
-        })
+.factory('compile', function (template, $sce) {
+  return function (parts) {
+    var code = template.replace(
+      /{{(.+?)}}/g,
+      function (_, type) {
+        return parts[type]
+      })
+    return $sce.trustAsHtml(code)
+  }
+})
 
-        .factory('save', function ($http) {
-            return function (parts) {
-                $http.post('/save', parts).then(function (response) {
-                    console.info(response);
-                }).catch(function (response) {
-                    console.error(response);
-                })
-            }
-        })
+.factory('save', function ($http) {
+  return function (parts) {
+    $http.post('/save', parts).then(function (response) {
+      console.info(response)
+    }).catch(function (response) {
+      console.error(response)
+      alert(response.config.data.html);
+      alert(response.config.data.javascript);
+      alert(response.config.data.css);
+    })
+  }
+})
 
-        .run(function ($rootScope, $templateRequest, compile, save) {
-            $rootScope.compile = compile;
-            $rootScope.save = save;
-            $templateRequest('init.html').then(function (data) {
-                $rootScope.html = data;
-            });
+.run(function ($rootScope, $templateRequest, compile, save) {
+  $rootScope.compile = compile
+  $rootScope.save = save
+  $templateRequest('init.html').then(function (data) {
+    $rootScope.html = data
+  })
 
-            $templateRequest('init.css').then(function (data) {
-                $rootScope.css = data;
-            });
+  $templateRequest('init.css').then(function (data) {
+    $rootScope.css = data
+  })
 
-            $templateRequest('init.js').then(function (data) {
-                $rootScope.javascript = data;
-            });
-        });
-
-
-angular.module('ucpen').controller('AccordionDemoCtrl', function ($scope) {
-    $scope.oneAtATime = true;
-
-    $scope.groups = [
-        {
-            title: 'Dynamic Group Header - 1',
-            content: 'Dynamic Group Body - 1'
-        },
-        {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
-        }
-    ];
-
-    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-    $scope.addItem = function () {
-        var newItemNo = $scope.items.length + 1;
-        $scope.items.push('Item ' + newItemNo);
-    };
-
-    $scope.status = {
-        isFirstOpen: true,
-        isFirstDisabled: false
-    };
-});
-
-
-
-
-
-
-
+  $templateRequest('init.js').then(function (data) {
+    $rootScope.javascript = data
+  })
+})
 
 
 
