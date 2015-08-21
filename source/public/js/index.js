481ba8efc5d0c27958232dea011a8b25b42443e7
iray198909820 <1853965225@qq.com>
2015-08-21 15:48:28 +0800

    UI修改
    改成2*2的显示模式
    项目下拉框改成了angular的交互模式
    左边的展开蓝现在还有bug

--- source/public/js/index.js
+++ source/public/js/index.js
@@ -1,50 +1,56 @@
-var template = angular.module('ucpen', [
-    'ngAnimate',
-    'ui.bootstrap',
-    'ui.codemirror'
-]);
+var template =
 
-template.value('template', '<!DOCTYPE html><html><head><meta charset="utf-8"><style>{{css}}</style></head><body>{{html}}<script>{{javascript}}</script></body></html>')
+    angular.module('ucpen', [
+        'ngAnimate',
+        'ui.bootstrap',
+        'ui.codemirror'
+    ])
 
-    .factory('compile', function (template, $sce) {
-        return function (parts) {
-            var code = template.replace(
-                /{{(.+?)}}/g,
-                function (_, type) {
-                    return parts[type]
-                });
-            return $sce.trustAsHtml(code)
-        }
-    })
+.value('template', '<!DOCTYPE html><html><head><meta charset="utf-8"><style>{{css}}</style></head><body>{{html}}<script>{{javascript}}</script></body></html>')
 
-    .factory('save', function ($http) {
-        return function (parts) {
-            $http.post('/save', parts).then(function (response) {
-                console.info(response)
-            }).catch(function (response) {
+.factory('compile', function(template, $sce) {
+    return function(parts) {
+        var code = template.replace(
+            /{{(.+?)}}/g,
+            function(_, type) {
+                return parts[type];
+            });
+        return $sce.trustAsHtml(code);
+    };
+})
+
+.factory('save', function($http) {
+        return function(parts) {
+            $http.post('/save', parts).then(function(response) {
+                console.info(response);
+            }).catch(function(response) {
                 console.error(response);
-                alert(response.config.data.html);
-                alert(response.config.data.javascript);
-                alert(response.config.data.css);
-            })
-        }
+                // alert(response.config.data.html);
+                // alert(response.config.data.javascript);
+                // alert(response.config.data.css);
+            });
+        };
+    })
+    .controller('projectList', function($scope) {
+        $scope.proList = '爱书旗 神马搜索 UC优视 商搜 框计算 阿里巴巴 阿里云 淘宝 支付宝 移动事业群 蚂蚁金服 阿里影业 菜鸟 国际事业部'.split(' ');
     })
 
-    .run(function ($rootScope, $templateRequest, compile, save) {
-        $rootScope.compile = compile;
-        $rootScope.save = save;
-        $templateRequest('init.html').then(function (data) {
-            $rootScope.html = data
-        });
+.controller('animateList', function($scope) {
+    $scope.aniList = ["样例1", "样例2", "样例3", "样例4"];
+})
 
-        $templateRequest('init.css').then(function (data) {
-            $rootScope.css = data
-        });
-
-        $templateRequest('init.js').then(function (data) {
-            $rootScope.javascript = data
-        });
+.run(function($rootScope, $templateRequest, compile, save) {
+    $rootScope.compile = compile;
+    $rootScope.save = save;
+    $templateRequest('init.html').then(function(data) {
+        $rootScope.html = data;
     });
 
+    $templateRequest('init.css').then(function(data) {
+        $rootScope.css = data;
+    });
 
-
+    $templateRequest('init.js').then(function(data) {
+        $rootScope.javascript = data;
+    });
+});
