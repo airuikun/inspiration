d8f55d08e11ef433cf2c28bf6a724de9c043d0dd
hacke2 <hacke2@qq.com>
2015-08-26 14:24:42 +0800

    新增路由

---
+++ config/Route.js
@@ -0,0 +1,26 @@
+var ComponentController = require('../controllers/ComponentController'),
+    CategoryController = require('../controllers/CategoryController'),
+    multipart = require('connect-multiparty'),
+    multipartMiddleware = multipart();
+
+
+exports.use = function (app) {
+
+    // http://yijiebuyi.com/blog/90c1381bfe0efb94cf9df932147552be.html
+    //表单默认提交方式为www-form-urlencoded
+    //有文件传输的为form-data类型，需要引入connect-multiparty库
+    //组件类的路由
+    app.post('/component/create', multipartMiddleware, ComponentController.create);
+    app.post('/component/edit', multipartMiddleware, ComponentController.edit);
+    app.get('/component/category/:categoryID', ComponentController.getComponentByCategoryID);
+
+    //类别
+    app.post('/category/create', CategoryController.createCategory);
+    app.get('/category/:productLineID', CategoryController.getCategoriesByProductLineID);
+
+
+    //产品线
+    app.post('/produceLine/create', CategoryController.createProductLine);
+
+
+};
