3a87eb18a2cc0bd2e60f739d7384e2ab14f25abe
zhengdawei <zhengdawei@ishuqi.com>
2015-09-16 21:10:30 +0800

    sass字符串转化为css字符串接口功能

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 var ComponentController = require('../controllers/ComponentController'),
     CategoryController = require('../controllers/CategoryController'),
+    OperationController = require('../controllers/OperationController'),
     //http://yijiebuyi.com/blog/90c1381bfe0efb94cf9df932147552be.html
     //表单默认提交方式为www-form-urlencoded
     //有文件传输的为form-data类型，需要引入connect-multiparty库
@@ -7,6 +8,7 @@ var ComponentController = require('../controllers/ComponentController'),
     multipartMiddleware = multipart();
 
 module.exports = function(app) {
+
     // 首页
     app.get('/', ComponentController.renderIndexPage);
     // 编辑组件页面
@@ -25,4 +27,9 @@ module.exports = function(app) {
 
     //产品线
     app.post('/productLine/create', CategoryController.createProductLine);
+
+    //操作功能类 add by zdw 15.09.16
+    // var bodyParser = require('body-parser');
+    // app.use(bodyParser.text());//从body获取内容时需使用此方式
+    app.post('/sa2css', OperationController.sass2css);
 };
